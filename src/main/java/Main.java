65f7249f436006f7253ecd5d488b81c4c5497cdc
Destinat0r <ashavrn@protonmail.com>
2019-12-15 14:20:05 +0200

    TaxiPark -> VehiclePark

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
 import model.entity.Vehicle;
+import model.entity.VehiclePark;
 import model.entity.impl.Car;
+import model.entity.impl.TaxiPark;
 import model.entity.impl.Truck;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -9,5 +14,13 @@ public class Main {
         Vehicle truck = new Truck("Toyota", 150, 25, 12000, 5000);
 
 
+        List<Vehicle> vehicleList = new ArrayList<>();
+        vehicleList.add(car);
+        vehicleList.add(truck);
+
+        VehiclePark taxiPark = new TaxiPark(vehicleList);
+        System.out.println(taxiPark.calculateTotalValue());
+        System.out.println(taxiPark.sortByFuelConsumptionAsc());
+        System.out.println(taxiPark.findVehiclesWithinMaxSpeedRange(100, 200));
     }
 }
